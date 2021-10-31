cdf3538fe52412400b0c9d2d030de96ea8cb20e6
arpit.s@surya-soft.com <arpit.s@surya-soft.com>
2021-10-31 20:22:54 +0530

    Initial Commit

---
+++ src/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { App } from './components/App';
+import { reducers } from './components/reducers';
+
+const store = createStore(reducers, applyMiddleware(thunk));
+
+interface AppProps {
+  color?: string;
+}
+
+// interface AppState {
+//   counter: number;
+// }
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  
+  document.querySelector('#root')
+);
